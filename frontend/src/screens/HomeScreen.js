1090375757797b0dc1c2a0e80451152c000ebe83
arnavgupta <arnavg.gupta@mail.utoronto.ca>
2021-01-19 14:12:29 +0530

    Added Redux to Home/Prod screen js. Created Store

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,40 +1,45 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useEffect } from 'react'
+// import {useState } from 'react'
+// import axios from 'axios'
 // import data from "../data";
 import Product from '../components/Product';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 export default function HomeScreen() {
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+    // const [products, setProducts] = useState([]);
+    // const [loading, setLoading] = useState(false);
+    // const [error, setError] = useState(false);
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products } = productList
+    const dispatch = useDispatch()
 
-    useEffect(() =>{
-        const fetchData = async () => {
-            try{
-                setLoading(true);
-                const { data } = await axios.get('/api/products');
-                setLoading(false);
-                setProducts(data);
-            } catch(err){
-                setError(err.message);
-                setLoading(false);
-            }
-        }
-        fetchData();
-    }, [])
-    
+    useEffect(() => {
+        // const fetchData = async () => {
+        //     try{
+        //         setLoading(true);
+        //         const { data } = await axios.get('/api/products');
+        //         setLoading(false);
+        //         setProducts(data);
+        //     } catch(err){
+        //         setError(err.message);
+        //         setLoading(false);
+        // fetchData();
+        dispatch(listProducts())
+    }, [dispatch])
+
     return (
         <div>
             {loading ? <LoadingBox></LoadingBox>
-            : error ? <MessageBox variant='danger'>{error}</MessageBox>
-            : <div className="row center">
-                { products.map((product) => (
-                    <Product key={product._id} product={product}></Product>
-                ))}
-            </div>
-             }
+                : error ? <MessageBox variant='danger'>{error}</MessageBox>
+                    : <div className="row center">
+                        {products.map((product) => (
+                            <Product key={product._id} product={product}></Product>
+                        ))}
+                    </div>
+            }
         </div>
     );
 }
