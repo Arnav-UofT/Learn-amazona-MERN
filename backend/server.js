1d79dcc0cc8f7438a7c663872eea2885c74d9a8a
arnavgupta <arnavg.gupta@mail.utoronto.ca>
2021-02-14 02:06:11 +0530

    FIX: err mongo resolved-services, promiseRejection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,20 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose'
+import userRouter from './routers/userRouter.js';
 
 const app = express();
+const uri = process.env.MONGODB_URL || 'mongodb://localhost/dataLink'
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+}).then(() => {
+    console.log('Mongodb successfully connected!')
+}).catch(err => {
+    console.log(err)
+}) ///SERVER IS NToOT WOKRING BECAUSE USERROUTER IS THROWING 
+/// SOME ERROR DUE TO PROMISE HANDLING
 
 app.get('/api/products/:id', (req, res) => {
     const product = data.products.find(x => x._id === req.params.id)
@@ -17,9 +30,16 @@ app.get('/api/products', (req, res) => {
     res.send(data.products)
 });
 
+app.use('/api/users', userRouter)
+
 app.get('/', (req, res) => {
     res.send('Server Ready!');
 });
+
+//error catching middle ware
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message })
+})
 
 // eslint-disable-next-line no-undef
 const port = process.env.port || 5000;
